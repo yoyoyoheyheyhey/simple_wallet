afacb46b1b12d5d1d455ad8fc6f8f4025d241679
Yohei Goto <yohei_goto@diveintocode.jp>
2020-11-09 22:18:05 +0900

    Securing the Smart Contract

--- SimpleWallet.sol
+++ SimpleWallet.sol
@@ -1,8 +1,19 @@
-pragma solidity >=0.4.22 <0.7.0;
+pragma solidity ^0.5.17;
 
 contract SimpleWallet {
     
-    function withdrawMoney(address payable _to, uint _amount) public {
+    address public owner;
+    
+    constructor() public {
+        owner = msg.sender;
+    }
+    
+    modifier onlyOwner() {
+        require(owner == msg.sender, "You're not the owner");
+        _;
+    }
+    
+    function withdrawMoney(address payable _to, uint _amount) public onlyOwner {
         _to.transfer(_amount);
     }
     
