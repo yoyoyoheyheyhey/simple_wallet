e801b2797434391e85036f6d413927006049130e
Yohei Goto <yohei_goto@diveintocode.jp>
2020-11-10 20:58:11 +0900

    Add Reduce-Allowance Functionality

--- SimpleWallet.sol
+++ SimpleWallet.sol
@@ -11,12 +11,20 @@ contract SimpleWallet is Ownable {
         allowance[_who] = _amount;
     }
     
-    modifier isAllowed(uint _amount) {
-        require(allowance[msg.sender] >= _amount, "You're not allowed");
+    modifier ownerOrAllowed(uint _amount) {
+        require(owner() == msg.sender || allowance[msg.sender] >= _amount, "You're not allowed");
         _;
     }
+    
+    function reduceAllowance(address _who, uint _amount) internal {
+        allowance[_who] -= _amount;
+    }
 
-    function withdrawMoney(address payable _to, uint _amount) public isAllowed(_amount) {
+    function withdrawMoneyTo(address payable _to, uint _amount) public ownerOrAllowed(_amount) {
+        require(_amount <= address(this).balance, "You don't have enough funds stored in the smart contract");
+        if(owner() != msg.sender) {
+            reduceAllowance(msg.sender, _amount);
+        }
         _to.transfer(_amount);
     }
     
