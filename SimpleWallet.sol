b346f5eead12fb95a2ff5bf7454c2d21e5166519
Yohei Goto <yohei_goto@diveintocode.jp>
2020-11-12 20:53:12 +0900

    Add Events for the Simple Wallet Smart Contract

--- SimpleWallet.sol
+++ SimpleWallet.sol
@@ -27,15 +27,19 @@ contract Allowance is Ownable {
 
 contract SimpleWallet is Allowance {
     
+    event MoneySent(address indexed _beneficiary, uint _amount);
+    event MoneyReceived(address indexed _from, uint _amount);
+    
     function withdrawMoneyTo(address payable _to, uint _amount) public ownerOrAllowed(_amount) {
         require(_amount <= address(this).balance, "You don't have enough funds stored in the smart contract");
         if(owner() != msg.sender) {
             reduceAllowance(msg.sender, _amount);
         }
+        emit MoneySent(_to, _amount);
         _to.transfer(_amount);
     }
     
     receive () external payable {
-        
+        emit MoneyReceived(msg.sender, msg.value);
     }
 }
